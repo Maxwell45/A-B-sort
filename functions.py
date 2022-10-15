e0cf5eed4b139bbefe32a3fcec7b61cbfa2dcd7c
maksim_salomatin <maxwell_d@inbox.ru>
2022-10-15 14:04:02 +0400

    Uploaded the code

---
+++ functions.py
@@ -0,0 +1,28 @@
+def id_start_at_zero(n_customers):
+    group_distribution = {}
+    for i in range(n_customers):
+        group = find_sum_of_digits(i)
+        if not group_distribution.get(group):
+            group_distribution[group] = 1
+            continue
+        group_distribution[find_sum_of_digits(i)] += 1
+    return group_distribution
+
+
+def id_start_at_n(n_customers, n_first_id):
+    group_distribution = {}
+    for i in range(n_first_id, n_first_id + n_customers):
+        group = find_sum_of_digits(i)
+        if not group_distribution.get(group):
+            group_distribution[group] = 1
+            continue
+        group_distribution[find_sum_of_digits(i)] += 1
+    return group_distribution
+
+
+def find_sum_of_digits(number):
+    digit_sum = 0
+    while number > 0:
+        digit_sum += number % 10
+        number = int(number / 10)
+    return digit_sum
---
+++ main.py
@@ -0,0 +1,4 @@
+import functions
+
+print(functions.id_start_at_zero(1685973))
+print(functions.id_start_at_n(5, 20))
