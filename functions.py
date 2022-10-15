bb28eb00dcfd6236a8bf202cc0911817437815aa
maksim_salomatin <maxwell_d@inbox.ru>
2022-10-15 15:57:36 +0400

    Removed redundancy

--- functions.py
+++ functions.py
@@ -1,12 +1,5 @@
 def id_start_at_zero(n_customers):
-    group_distribution = {}
-    for i in range(n_customers):
-        group = find_sum_of_digits(i)
-        if not group_distribution.get(group):
-            group_distribution[group] = 1
-            continue
-        group_distribution[find_sum_of_digits(i)] += 1
-    return group_distribution
+    return id_start_at_n(n_customers, 0)
 
 
 def id_start_at_n(n_customers, n_first_id):
